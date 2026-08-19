28ca5e8a03109f8a16d0f545644f1b97ab69e93f
Nathan Campos <nathan@innoveworkshop.com>
2026-08-19 17:21:25 +0100

    Created a clipboard item data class
    This class will be responsible for abstracting away any clipboard objecs
    that we have in our appication.

---
+++ Clip.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyStand
+{
+    /// <summary>
+    /// Data object containing an instance of a clipboard data copied event.
+    /// </summary>
+    class Clip
+    {
+        private DateTime _timestamp;
+        private string _data;
+        private string _device;
+
+        /// <summary>
+        /// Constructs a brand new clipboard data object with all parameters.
+        /// </summary>
+        /// <param name="timestamp">Timestamp of when the copy happened.</param>
+        /// <param name="data">Data that was copied to the clipboard.</param>
+        /// <param name="device">The device that originally copied the data to its clipboard.</param>
+        public Clip(DateTime timestamp, string data, string device)
+        {
+            this._timestamp = timestamp;
+            this._data = data;
+            this._device = device;
+        }
+
+        /// <summary>
+        /// Constructs a brand new clipboard data object that was copied by ourselves, right now.
+        /// </summary>
+        /// <param name="data">Data that was copied to the clipboard.</param>
+        public Clip(string data) : this(DateTime.Now, data, "localhost") {}
+
+        /// <summary>
+        /// When the clipboard event happened.
+        /// </summary>
+        public DateTime Timestamp
+        {
+            get { return this._timestamp; }
+            set { this._timestamp = value; }
+        }
+
+        /// <summary>
+        /// Data that was copied to the clipboard.
+        /// </summary>
+        public string Data
+        {
+            get { return this._data; }
+            set { this._data = value; }
+        }
+
+        /// <summary>
+        /// Device that originally copied the data to its clipboard.
+        /// </summary>
+        public string Device
+        {
+            get { return this._device; }
+            set { this._device = value; }
+        }
+    }
+}
