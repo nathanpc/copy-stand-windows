7b62bbcd68eb5deaaffe7f904927262dbee148f0
Nathan Campos <nathan@innoveworkshop.com>
2026-08-21 13:07:10 +0100

    Created a settings class to manage settings globally
    This class will help us keep our settings synced across the application
    and provide an easy way to fetch and update settings.

---
+++ Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyStand
+{
+    /// <summary>
+    /// Handles the application's global settings.
+    /// </summary>
+    public static class Settings
+    {
+        private static int _historyLimit;
+
+        /// <summary>
+        /// Initializes the settings with registry data, or defaults if registry
+        /// data is not available.
+        /// </summary>
+        static Settings()
+        {
+            _historyLimit = 50;
+        }
+
+        /// <summary>
+        /// Maximum number of clipboard items in our history list.
+        /// </summary>
+        public static int HistoryLimit
+        {
+            get { return _historyLimit; }
+            set { _historyLimit = value; }
+        }
+    }
+}
